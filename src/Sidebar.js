7ee2bcadba3cc6b712628de6861d0a604022ff11
Md Anas Sabah <mdanassabah@gmail.com>
2022-04-02 22:57:27 +0530

    Intial Stage

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Sidebar.css'
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar__top'>
+            <h3>Textelope Community</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
